1b9bf34a9ef4cef54b1b74dba468f9ce0602dffa
Robert Wagner <rwwagner90@gmail.com>
2016-11-17 13:54:59 -0500

    Add query params on top rule

--- config/recommended.js
+++ config/recommended.js
@@ -88,6 +88,7 @@ module.exports = {
     'ship-shape/no-direct-property-access': 'error',
     'ship-shape/no-side-effects': 'error',
     'ship-shape/prefer-destructuring': 'error',
+    'ship-shape/query-params-on-top': 'error',
     'ship-shape/require-access-in-comments': 'error',
     'ship-shape/require-const-for-ember-properties': 'error'
   }
